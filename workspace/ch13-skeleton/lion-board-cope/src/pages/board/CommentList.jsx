3e69ee1be975643859eed45b7886e5b6c07ea301
kim-young-dong <opa9904@gmail.com>
2024-12-09 18:07:18 +0900

    ✨ 댓글 목록 및 댓글 추가 기능 개선: PropTypes 추가, 댓글 수 동적 표시, 댓글 내용 및 작성자 정보 업데이트

--- workspace/ch13-skeleton/lion-board-cope/src/pages/board/CommentList.jsx
+++ workspace/ch13-skeleton/lion-board-cope/src/pages/board/CommentList.jsx
@@ -1,15 +1,21 @@
+import PropTypes from "prop-types";
 import CommentListItem from "@pages/board/CommentListItem";
 import CommentNew from "@pages/board/CommentNew";
 
-export default function CommentList() {
+CommentList.propTypes = {
+  comments: PropTypes.array,
+};
+
+export default function CommentList({ comments }) {
   return (
     <section className="mb-8">
-      <h4 className="mt-8 mb-4 ml-2">댓글 2개</h4>
+      <h4 className="mt-8 mb-4 ml-2">댓글 {comments.length}개</h4>
 
-      <CommentListItem />
+      {comments.map((comment) => (
+        <CommentListItem key={comment["_id"]} comment={comment} />
+      ))}
 
       <CommentNew />
-
     </section>
   );
 }
--- workspace/ch13-skeleton/lion-board-cope/src/pages/board/ListItem.jsx
+++ workspace/ch13-skeleton/lion-board-cope/src/pages/board/ListItem.jsx
@@ -17,12 +17,20 @@ ListItem.propTypes = {
 export default function ListItem({ item }) {
   return (
     <tr className="border-b border-gray-200 hover:bg-gray-200 dark:hover:bg-gray-700 transition duration-300 ease-in-out">
-      <td className="p-2 text-center">{ item._id }</td>
-      <td className="p-2 truncate indent-4"><Link to={`${ item._id }`} className="cursor-pointer">{ item.title }</Link></td>
-      <td className="p-2 text-center truncate">{ item.user.name }</td>
-      <td className="p-2 text-center hidden sm:table-cell">{ item.views }</td>
-      <td className="p-2 text-center hidden sm:table-cell">{ item.repliesCount }</td>
-      <td className="p-2 truncate text-center hidden sm:table-cell">{ item.createdAt }</td>
+      <td className="p-2 text-center">{item._id}</td>
+      <td className="p-2 truncate indent-4">
+        <Link to={`${item._id}`} className="cursor-pointer">
+          {item.title}
+        </Link>
+      </td>
+      <td className="p-2 text-center truncate">{item.user.name}</td>
+      <td className="p-2 text-center hidden sm:table-cell">{item.views}</td>
+      <td className="p-2 text-center hidden sm:table-cell">
+        {item.repliesCount}
+      </td>
+      <td className="p-2 truncate text-center hidden sm:table-cell">
+        {item.createdAt}
+      </td>
     </tr>
   );
 }

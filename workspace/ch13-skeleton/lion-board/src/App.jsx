98cd5e46ffc2e1530241d292a9d42862d8fe7d88
kim-young-dong <opa9904@gmail.com>
2024-12-12 11:27:52 +0900

    ✨ App 컴포넌트에 HelmetProvider 추가 및 Detail 페이지에 메타 태그 설정

--- workspace/ch13-skeleton/lion-board/src/App.jsx
+++ workspace/ch13-skeleton/lion-board/src/App.jsx
@@ -1,20 +1,22 @@
 import { RouterProvider } from "react-router-dom";
-import router from '@/routes';
+import router from "@/routes";
 import useThemeStore from "@zustand/themeStore";
+import { HelmetProvider } from "react-helmet-async";
 
 function App() {
-
   const { isDarkMode } = useThemeStore();
 
-  if(isDarkMode){
-    document.documentElement.classList.add('dark');
-  }else{
-    document.documentElement.classList.remove('dark');
+  if (isDarkMode) {
+    document.documentElement.classList.add("dark");
+  } else {
+    document.documentElement.classList.remove("dark");
   }
-  
+
   return (
-    <RouterProvider router={router} future={{ v7_startTransition: true }} />
+    <HelmetProvider>
+      <RouterProvider router={router} future={{ v7_startTransition: true }} />
+    </HelmetProvider>
   );
 }
 
-export default App
+export default App;
